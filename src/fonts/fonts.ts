68d845ae1a3bad9e4ebb840efdb7070ac106ff98
gianlucagalota <gianluca.galota@gmail.com>
2023-12-22 11:07:02 +0100

    set adjust font fallback to false

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -3,4 +3,5 @@ import { Instrument_Sans } from 'next/font/google';
 export const instrumentSans = Instrument_Sans({
   subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
+  adjustFontFallback: false,
 });
