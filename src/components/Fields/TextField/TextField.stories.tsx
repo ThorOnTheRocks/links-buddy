5dff1f4c933c2ee8fe973716883e558a60f4b967
gianlucagalota <gianluca.galota@gmail.com>
2023-12-22 08:53:39 +0100

    added render with hook on TextField story

--- src/components/Fields/TextField/TextField.stories.tsx
+++ src/components/Fields/TextField/TextField.stories.tsx
@@ -1,7 +1,8 @@
-import React, { ChangeEvent, ReactNode, useState } from 'react';
+import React, { ChangeEvent, useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { LinkIcon } from '@heroicons/react/16/solid';
 import { TextField } from './TextField';
+import { ITextFieldProps } from './TextField.types';
 
 const meta: Meta<typeof TextField> = {
   title: 'Fields/TextField',
@@ -11,32 +12,57 @@ const meta: Meta<typeof TextField> = {
 export default meta;
 type Story = StoryObj<typeof TextField>;
 
+const TextFieldWithHooks = ({
+  type,
+  name,
+  value,
+  onChange,
+  ...props
+}: ITextFieldProps) => {
+  const [text, setText] = useState('');
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+  return (
+    <TextField
+      value={text}
+      onChange={handleChangeInput}
+      type={type}
+      name={name}
+      {...props}
+    />
+  );
+};
+
 export const TextFieldEmpty: Story = {
   args: {
-    value: '',
+    placeholder: 'Text Field Empty',
     name: 'empty',
     onChange: (e: ChangeEvent<HTMLInputElement>) => e.target.value,
     label: 'Empty TextField:',
   },
+  render: ({ ...args }) => <TextFieldWithHooks {...args} />,
 };
 
 export const TextFieldErrorState: Story = {
   args: {
-    value: 'Error Value',
+    placeholder: 'Error Value',
     name: 'error',
     isError: true,
-    onChange: (e: ChangeEvent<HTMLInputElement>) => e.target.value,
     label: 'Error TextField:',
     icon: <LinkIcon />,
   },
+  render: ({ ...args }) => <TextFieldWithHooks {...args} />,
 };
 
 export const TextFieldWithIcon: Story = {
   args: {
-    value: '',
+    placeholder: 'Text Field With Icon',
     name: 'icon',
     onChange: (e: ChangeEvent<HTMLInputElement>) => e.target.value,
     label: 'TextField with Icon:',
     icon: <LinkIcon />,
   },
+  render: ({ ...args }) => <TextFieldWithHooks {...args} />,
 };
