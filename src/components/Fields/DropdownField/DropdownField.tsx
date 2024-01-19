e6887185b9abb50ce79e55ce2076c988e3d14889
gianlucagalota <gianluca.galota@gmail.com>
2024-01-19 16:37:48 +0100

    added dropdown component types

--- src/components/Fields/DropdownField/DropdownField.tsx
+++ src/components/Fields/DropdownField/DropdownField.tsx
@@ -1,16 +1,35 @@
 'use client';
 
-import styles from './text-field.module.css';
+import { useState } from 'react';
+import styles from './dropdown-field.module.css';
 import type { IDropdownFieldProps } from './DropdownField.types';
+import {
+  ChevronUpIcon,
+  ChevronDownIcon,
+} from '@heroicons/react/16/solid';
 
 export const DropdownField = ({
-  type,
   icon,
 }: IDropdownFieldProps): JSX.Element => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleToggleDropdown = () => {
+    setIsOpen((prev) => !prev);
+  };
+
   return (
-    <div className={styles.containerTextField}>
-      <div className={styles.containerInputTextField}>
-        {icon && <div>{icon}</div>}
+    <div
+      className={styles.containerDropdownField}
+      onClick={handleToggleDropdown}
+    >
+      <div className={styles.containerDropdownFieldText}>
+        {icon && (
+          <div className={styles.iconDropdownField}>{icon}</div>
+        )}
+        <p>placeholder</p>
+        <div className={styles.iconToggleDropdownField}>
+          {isOpen ? <ChevronUpIcon /> : <ChevronDownIcon />}
+        </div>
       </div>
     </div>
   );
