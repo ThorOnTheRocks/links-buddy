d194796444ee93620d5fdb5098ee739c947aae2d
gianlucagalota <gianluca.galota@gmail.com>
2024-06-28 23:51:05 +0200

    renaming file

---
+++ src/schema/EmailSubscriptionSchema.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+
+export const EmailSubscriptionSchema = z
+  .object({
+    email: z
+      .string()
+      .min(1, { message: 'Email is required' })
+      .email({ message: 'Invalid email address' }),
+  })
+  .required();
