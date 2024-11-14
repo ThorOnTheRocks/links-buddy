2aee48c9ab950a8e87e8e6f1aac7f73324a3d211
gianlucagalota <gianluca.galota@gmail.com>
2024-11-14 14:09:57 +0100

    feat: unit test  User and Auth entities

---
+++ src/core/mocks/Auth.mock.ts
@@ -0,0 +1,11 @@
+import { User } from '../entities/User/User';
+
+export const user: User = {
+  id: '423345',
+  firstName: 'John',
+  lastName: 'Doe',
+  email: 'john.doe@gmail.com',
+  username: 'therealjohndoe',
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
